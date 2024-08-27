7b0555fa8cb231c8cce7d800fca5a3bd654a37a0
Dirceusljr <dirceusljr@gmail.com>
2024-08-27 20:36:53 -0300

    feat: :sparkles: feat: TypePet

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
+import type TypePet from "../types/TypePet";
 
-const listaPet = [];
+const listaPet: TypePet[] = [];
 
 export default class PetController {
     criaPet(req: Request, res: Response) {
-        const novoPet = req.body;
+        const {id, nome, especie, idade, adotado } = <TypePet>req.body;
+        const novoPet: TypePet = { id, nome, especie, idade, adotado };
         listaPet.push(novoPet);
         return res.status(201).json(novoPet);
     }
---
+++ src/types/TypePet.ts
@@ -0,0 +1,9 @@
+type TypePet = {
+    id: number,
+    nome: string,
+    especie: string,
+    idade: number,
+    adotado: boolean
+}
+
+export default TypePet;
