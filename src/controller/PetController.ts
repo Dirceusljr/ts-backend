8d0a934c7b97855f92547c197f8d41e58dcba266
Dirceusljr <dirceusljr@gmail.com>
2024-08-27 21:08:03 -0300

    feat: :sparkles: feat: CRUD Pets

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -10,4 +10,37 @@ export default class PetController {
         listaPet.push(novoPet);
         return res.status(201).json(novoPet);
     }
+
+    listaPets(req: Request, res: Response) {
+        return res.status(200).json(listaPet);
+    }
+
+    atualizaPet(req: Request, res: Response) {
+        const { id } = req.params;
+        const { nome, especie, adotado, idade } = req.body as TypePet;
+
+        const pet = listaPet.find((pet) => pet.id === Number(id));
+        if(!pet) {
+            return res.status(404).json({ message: "Pet não encontrado." });
+        }
+
+        pet.nome = nome;
+        pet.especie = especie;
+        pet.adotado = adotado;
+        pet.idade = idade;
+
+        return res.status(200).json(pet);
+    }
+
+    deletaPet(req: Request, res: Response) {
+        const { id } = req.params;
+        const pet = listaPet.find((pet) => pet.id === Number(id));
+        if(!pet) {
+            return res.status(404).json({ message: "Pet não encontrado." });
+        }
+
+        const index = listaPet.indexOf(pet);
+        listaPet.splice(index, 1);
+        return res.status(200).json({ message: "Pet deletado com sucesso." });
+    }
 }
