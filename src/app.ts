26d1cf1dcd9145e4534c1207a8a14aaa18f31198
Dirceusljr <dirceusljr@gmail.com>
2024-08-29 09:46:25 -0300

    feat: :sparkles: feat: add database

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
-import express, { Response } from "express";
+import express from "express";
 import router from "./routes";
+import "reflect-metadata"
+import { appDataSource } from "./config/dataSource";
 
 const app = express();
 app.use(express.json());
-
 router(app);
+
+appDataSource.initialize()
+    .then(() => {
+        console.log('Banco de dados conectado.');
+    })
+    .catch((erro) => {
+        console.log(erro)
+    })
 
 export default app;
