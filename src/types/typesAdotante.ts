524bdb441b8ed9afc260b9def37dc97477f7dcb0
Dirceusljr <dirceusljr@gmail.com>
2024-09-02 22:02:46 -0300

    feat: :sparkles: feat: Body validator using Yup

--- src/types/typesAdotante.ts
+++ src/types/typesAdotante.ts
@@ -1,11 +1,11 @@
 import AdotanteEntity from "../entities/AdotanteEntity";
 
-type TypeRequestBodyAdotante = Omit<AdotanteEntity, "id">;
+type TypeRequestBodyAdotante = Omit<AdotanteEntity, "id" | "pets">;
 
 type TypeRequestParamsAdotante = { id?: string };
 
 type TypeResponseBodyAdotante = {
-    data?: Pick<AdotanteEntity, "id" | "nome" | "celular"> | Pick<AdotanteEntity, "id" | "nome" | "celular">[],
+    data?: Pick<AdotanteEntity, "id" | "nome" | "celular" | "endereco"> | Pick<AdotanteEntity, "id" | "nome" | "celular" | "endereco">[],
     erro?: unknown;
 }
 
